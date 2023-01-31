8d6887ea83cb62a4126e8ee06f30b9c4c97548ff
Ivo Rosenberger <ivorosenberger@gmail.com>
2023-01-31 15:21:56 -0300

    2da-entrega

---
+++ src/routers/carritosRouter.js
@@ -0,0 +1,49 @@
+import { Router } from "express";
+import { carritosDao, productosDao } from "../daos/index.js";
+
+const carritosRouter = Router();
+
+//configuracion del carrito
+carritosRouter.post("/", async (req, res) => {
+    const carroAgregado = await carritosDao.guardar(req.body);
+    res.json(carroAgregado);
+});
+
+carritosRouter.get("/", async (req, res) => {
+    const carritos = await carritosDao.listarAll();
+    res.json(carritos);
+});
+
+carritosRouter.delete("/:id", async (req, res) => {
+    const carroEliminado = await carritosDao.borrar(req.params.id);
+    res.json(carroEliminado);
+});
+
+
+//productos en el carrito:
+
+carritosRouter.get('/:id/productos', async (req, res) => {
+    const carrito = await carritosDao.listar(req.params.id)
+    res.json(carrito.productos)
+})
+
+carritosRouter.post('/:id/productos', async (req, res) => {
+    const carrito = await carritosDao.listar(req.params.id)
+    const producto = await productosDao.listar(req.body.id)
+    carrito.productos.push(producto)
+    await carritosDao.actualizar(carrito)
+    res.end()
+})
+
+carritosRouter.delete('/:id/productos/:idProd', async (req, res) => {
+    const carrito = await carritosDao.listar(req.params.id)
+    const index = carrito.productos.findIndex(p => p.id == req.params.idProd)
+    if (index != -1) {
+        carrito.productos.splice(index, 1)
+        await carritosDao.actualizar(carrito)
+    }
+    res.end()
+})
+
+
+export { carritosRouter };
--- src/server.js
+++ src/server.js
@@ -1,18 +1,29 @@
 import express from "express";
-import productRouter from './routes/product.js';
-import cartRouter from './routes/cart.js';
+import { productosRouter } from "./routers/productosRouter.js";
+import { carritosRouter } from "./routers/carritosRouter.js";
+import morgan from "morgan";
 
-const PORT = 3026;
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/productos', productRouter);
-app.use('/api/carrito', cartRouter);
+app.use(morgan("dev"));
+app.use("/api/productos", productosRouter);
+app.use("/api/carritos", carritosRouter);
 
-const server = app.listen(PORT, () => {
-    console.log(` >>>>> 🚀 Server started at http://localhost:${PORT}`)
-    })
-    
-server.on('error', (err) => console.log(err));
+
+app.use("/productos", (req, res) => {
+    res.render("productos");
+});
+app.use("/carritos", (req, res) => {
+    res.render("carritos");
+});
+app.get("*", (req, res) => {
+    res.render("home");
+});
+
+export default app;
